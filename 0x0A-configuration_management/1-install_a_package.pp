d43172305b749592b0fdce98534d3439288367e2
kipngeno kevin <kipngenokevin254@gmail.com>
2024-01-19 16:00:21 +0000

    task 1 update

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,6 @@
 # Install Flask version 2.1.0 using pip3
 
-exec { 'install_flask':
-  command => '/usr/bin/pip3 install Flask==2.1.0',
+package { 'flask':
+  ensure   => '2.1.0',
+  provider => 'pip3',
 }
