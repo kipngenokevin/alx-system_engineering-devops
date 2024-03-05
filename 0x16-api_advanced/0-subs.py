48d2930e44f1f437ec3624d534c5b8603d68e42d
kipngeno kevin <kipngenokevin254@gmail.com>
2024-03-05 16:46:45 +0000

    check the number of subscribers

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -2,10 +2,22 @@
 """module with a function that queries the Reddit API
 and returns the number of subscribers
 """
-import requests
+from requests import get
 
 
 def number_of_subscribers(subreddit):
-	"""returns the number of subscribers
-	for a given subreddit.
-	"""
+    """returns the number of subscribers
+    for a given subreddit.
+    """
+    if subreddit is None or not isinstance(subreddit, str):
+        return 0
+
+    headers = {'User-Agent': 'Mozilla/5.0'}
+    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
+    response = get(url, headers=headers)
+    all_data = response.json()
+
+    try:
+        return all_data.get('data').get('subscribers')
+    except:
+        return 0
