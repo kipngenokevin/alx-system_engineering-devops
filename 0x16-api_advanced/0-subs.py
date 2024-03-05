63b8259eae636cae78777d7a0939ce4f513fc25e
kipngeno kevin <kipngenokevin254@gmail.com>
2024-03-05 16:55:29 +0000

    Reddit subscribers

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -16,10 +16,9 @@ def number_of_subscribers(subreddit):
 
     headers = {'User-Agent': 'Mozilla/5.0'}
     url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
-    response = get(url, headers=headers)
-    all_data = response.json()
+    response = get(url, headers=headers, allow_redirects=False)
 
-    try:
-        return all_data.get('data').get('subscribers')
-    except Exception as e:
+    if response.status_code >= 300:
         return 0
+
+    return response.json().get('data').get('subscribers')
